52d8fbff4b9fe51b244cdb656e376e005e12aff5
mauriciovancine <mauricio.vancine@gmail.com>
2025-05-11 16:25:27 -0300

    update blog and teaching

--- teaching/workshop-git/hexagon/sticker.R
+++ teaching/workshop-git/hexagon/sticker.R
@@ -4,15 +4,15 @@
 library(hexSticker)
 library(png)
 
-imgurl <- magick::image_read("content/teaching/workshop-git/hexagon/img.png")
+imgurl <- magick::image_read("teaching/workshop-git/hexagon/img.png")
 
 sticker(imgurl, 
-        package = "git", 
+        package = "gitR", 
         p_size = 25, 
         p_color = "gray30",
         s_x = 1, s_y = .65,
         s_width = 1, s_height = 1,
         h_fill = "#dbe3e2",
         h_color = "#ff453c",
-        filename = "content/teaching/workshop-git/featured-hex.png")
+        filename = "teaching/workshop-git/featured-hex.png")
 
