c725f1b57a6e2ef7dc62217ea05d6cbfbee5f1ac
mauriciovancine <mauricio.vancine@gmail.com>
2025-05-25 16:53:37 -0300

    update publications and teaching

---
+++ teaching/course-ecostatr/hexagon/sticker.R
@@ -0,0 +1,19 @@
+
+# remotes::install_github("GuangchuangYu/hexSticker")
+
+library(hexSticker)
+library(magick)
+
+imgurl <- magick::image_read_svg("teaching/course-ecostatr/hexagon/r_logo.svg")
+
+hexSticker::sticker(
+  subplot = imgurl, 
+  package = "ecostatR", 
+  p_size = 25, 
+  p_x = 1, p_y = 1.45, 
+  p_color = "gray30",
+  s_x = .94, s_y = .75, 
+  s_width = 1.1, s_height = 1.1,
+  h_fill = "#dbe3e2",
+  h_color = "#87b13f",
+  filename = "teaching/course-ecostatr/featured-hex.png")
