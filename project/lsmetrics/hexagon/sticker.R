e7fb353ba654fcd902c57613d23752d4b8ddc0f4
mauriciovancine <mauricio.vancine@gmail.com>
2025-08-01 08:14:24 -0300

    multiple corrections

--- project/lsmetrics/hexagon/sticker.R
+++ project/lsmetrics/hexagon/sticker.R
@@ -4,15 +4,15 @@
 library(hexSticker)
 library(png)
 
-imgurl <- magick::image_read('content/project/lsmetrics/hexagon/img.png')
+imgurl <- magick::image_read('project/lsmetrics/hexagon/img.png')
 
 sticker(imgurl, 
-        package = "LSmetrics", 
+        package = "lsmetrics", 
         p_size = 20, 
         p_color = "#4f7476",
         s_x = 1, s_y = .8, s_width = 1.2, s_height = 1.2,
         h_fill = "#ffd445",
         #h_fill = "white",
         h_color = "#006821",
-        filename = "content/project/lsmetrics/featured-hex.png")
+        filename = "project/lsmetrics/featured-hex.png")
 
