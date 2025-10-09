6256292452b5b93e18cbdfae46ef8f5b4714fc14
mauriciovancine <mauricio.vancine@gmail.com>
2025-10-08 22:59:29 -0300

    update teaching

---
+++ course/course-reproductibility/hexagon/sticker.R
@@ -0,0 +1,19 @@
+
+# remotes::install_github("GuangchuangYu/hexSticker")
+
+library(hexSticker)
+library(magick)
+
+imgurl <- magick::image_read_svg("course/course-reproductibility/hexagon/r_logo.svg")
+
+hexSticker::sticker(
+  subplot = imgurl, 
+  package = "ecoReproduc", 
+  p_size = 20, 
+  p_x = 1, p_y = 1.45, 
+  p_color = "gray30",
+  s_x = .94, s_y = .75, 
+  s_width = 1.1, s_height = 1.1,
+  h_fill = "#dbe3e2",
+  h_color = "#87b13f",
+  filename = "course/course-reproductibility/featured-hex.png")
